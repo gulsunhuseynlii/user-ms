bb582d9bd663992f71ec6bed0b45241fda403bf2
gulsunhuseynlii <gulsunph-apa104@code.edu.az>
2026-06-30 09:47:16 +0400

    swagger

--- src/main/java/com/gulsun/user_ms/config/SecurityConfig.java
+++ src/main/java/com/gulsun/user_ms/config/SecurityConfig.java
@@ -20,6 +20,11 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().permitAll()
                 )
                 .build();
